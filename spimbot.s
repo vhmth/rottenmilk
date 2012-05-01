820b4f61998ff036f11931b76337cbe7386e5b71
Vinay <vhiremath4@gmail.com>
2012-04-30 21:32:27 -0500

    Got kicking done.

--- spimbot.s
+++ spimbot.s
@@ -13,6 +13,7 @@
 .data
     num_puzzles:    .word 0
     ball_to_kick:   .word -1
+    goal_x          .word 0           # default to the left goal
 
 main:
 
@@ -20,9 +21,18 @@ main:
     li   $t4, 0x6001                  # enable suduko and kick
     mtc0 $t4, $12
 
-    # set up some of our variables
-    sw $t1, num_puzzles
-    sw $t2, ball_to_kick
+    # set up our stack variables
+    sub  $sp, $sp, 4
+    sw   $s0, 0($sp)
+
+    # figure out which goal we need to score on
+    sw   $t0, 0xffff0020($0)
+    li   $t3, 150
+    bgt  $t0, $t3, main_while
+
+    # we shoot for the right goal
+    li   $t0, 300
+    lw   $t0, goal_x
 
 main_while:
     # 1.) check if num_puzzles is zero
@@ -38,12 +48,41 @@ main_while:
     #       check if ball is kickable
     #       if (yes):
     #           kick ball
+    sw   $t2, ball_to_kick
+    bltz $t2, runto_main              # if there are no balls to kick
 
+    sw   $t2, 0xffff00d0($0)          # select ball
+    lw   $t3, 0xffff00e0($0)          # check if ball exists
+    beq  $t3, 0, runto_main
+
+    lw   $t3, 0xffff00e4($0)          # check if ball is kickable
+    beq  $t3, 0, runto_main
+
+    lw   $t0, 0xffff00d4($0)          # get angle of ball to goal
+    lw   $t1, 0xffff00d8($0)
+    sw   $t2, goal_x
+    li   $t3, 150
+    sub  $t0, $t0, $t2                # delta x
+    sub  $t1, $t1, $t3                # delta y
+    move $a0, $t0
+    move $a1, $t1
+
+    move $s0, $ra
+    jal  arctan
+    move $ra, $s0
+    move $t0, $v0                     # angle from arctan
+    sub  $t0, $t0, 180
+    sw   $t0, 0xffff00c4($0)          # set kick orientation
+    li   $t0, 50
+    sw   $t0, 0xffff00c8($0)          # kick with 50 energy
+
+runto_main:
     # 3.) check closest ball that exists on field
     #
     #     calculate arc tan to ball
     #     run towards ball
 
+end_while:
     # 4.) jump to top of loop
     j main_while
 
